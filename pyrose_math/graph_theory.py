af9e6ec675e0452ce64269bd57ff2b191b7ca55a
Ben Caunt <bdcaunt@gmail.com>
2023-08-11 16:22:50 -0400

    weird issue where cycle detection sometimes hits a subscriber and everything fails

--- pyrose_math/graph_theory.py
+++ pyrose_math/graph_theory.py
@@ -57,9 +57,6 @@ def is_cycle_present(connected_topics: list) -> bool:
     hare = connected_topics[0]
 
     while True:
-        # TODO: this is horrible 
-        return False
-
         if not isinstance(hare, Topic):
             return False
 
